99e051d7f32ac1b3db16f1ab01f38540bb6a05f2
du <2554163051@qq.com>
2022-08-17 10:04:45 +0800

    first commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.study.git;
+
+/**
+ * @author dwj
+ * @date 2022-08-17-9:53
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+        String a;
+
+    }
+}
